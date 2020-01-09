c50a0ec827ef3a52c6730d4fee5286098c44478c
Ankit Vats <ankitvats13@gmail.com>
2020-01-09 17:35:48 +0530

    count animation

--- js/scripts.js
+++ js/scripts.js
@@ -58,6 +58,42 @@ jQuery(window).bind("debouncedresize", function() {
 });
 
 /* ---------------------------------------------------------------------------
+ * Number Count
+ * --------------------------------------------------------------------------- */
+var initialVal = 0;
+$(window).scroll(function() {
+
+  var oTop = $('#counter').offset().top - window.innerHeight;
+  if (initialVal == 0 && $(window).scrollTop() > oTop) {
+    $('.counter-value').each(function() {
+      var $this = $(this),
+        countTo = $this.attr('data-count');
+      $({
+        countNum: $this.text()
+      }).animate({
+          countNum: countTo
+        },
+
+        {
+
+          duration: 4000,
+          easing: 'swing',
+          step: function() {
+            $this.text(Math.floor(this.countNum));
+          },
+          complete: function() {
+            $this.text(this.countNum);
+            //alert('finished');
+          }
+
+        });
+    });
+    initialVal = 1;
+  }
+
+});
+
+/* ---------------------------------------------------------------------------
  * Zoom Box | Vertical Align
  * --------------------------------------------------------------------------- */
 function zoomBoxVerticalAlign() {
